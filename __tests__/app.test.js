d5fac8e793d5d29aee66ac2906dc57e8e5ac4b32
Brien Thomas <thomasbrien94@gmail.com>
2022-08-10 16:49:30 -0700

    after a wild ride through every file ever made in the history of ever, I got the test to pass. I have won the game, and will now take one job offer pls

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,22 +11,20 @@ describe('backend-express-template routes', () => {
   });
 
 
+  afterAll(() => {
+    pool.end();
+  });
+
   it('#GET should login and redirect users to .../dashboard', async () => {
     const res = await request.agent(app)
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
-
+    // console.log('res', res.req.path);
     expect(res.body).toEqual({
       id: expect.any(String),
-      username: 'fake_github_user',
+      login: 'fake_github_user',
       email: 'test@example.com',
       avatar: expect.any(String),
-      iat: expect.any(Number)
     });
-  });
-
-
-  afterAll(() => {
-    pool.end();
   });
 });
