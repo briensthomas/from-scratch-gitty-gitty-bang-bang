dcbbc620b8da01ddb3b5b32e1ab8a47f0a9628f2
Brien Thomas <thomasbrien94@gmail.com>
2022-08-10 12:13:45 -0700

    added first test which expects to see a fake response from mock, matching the data structure of what would be returned for an actual user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,29 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+
+  it('#GET should login and redirect users to .../dashboard', async () => {
+    const res = await request.agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'test@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number)
+    });
   });
+
+
   afterAll(() => {
     pool.end();
   });
