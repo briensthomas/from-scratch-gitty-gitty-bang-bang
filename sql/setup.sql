4d4b4ec62c3f93703d6cc2d9e26784f171295d2a
Brien Thomas <thomasbrien94@gmail.com>
2022-08-10 17:18:05 -0700

    SQL table for posts setup, Model for Post created and the getAll function is written to return all entries of the posts table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE github_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,3 +9,18 @@ CREATE TABLE github_users (
     email TEXT,
     avatar TEXT
 );
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+INSERT INTO posts (
+    title, 
+    description
+)
+VALUES 
+('BBQ at Rigby''s!', 'Feel free to bring any dishes you''d like, will update Slack post if we need anything specific'),
+('Do you prefer front-end, or back-end?', 'Just curious what y''all are enjoying more, or maybe which architecture you clicked better with.');
